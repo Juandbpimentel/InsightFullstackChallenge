67d16d890a5a9679910cc4880db83a725a06e3d5
Juan Pimentel <juandbpimentel@alu.ufc.br>
2024-06-03 17:31:16 -0300

    fix: corrigindo problema do container do backend

--- backend/src/main/java/br/ufc/quixada/SupplierApplicationInsight/Main.java
+++ backend/src/main/java/br/ufc/quixada/SupplierApplicationInsight/Main.java
@@ -1,14 +1,15 @@
 package br.ufc.quixada.SupplierApplicationInsight;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class Application {
+public class Main {
 	public static void main(String[] args) {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
 		builder.run(args);
 		System.out.println("The API is up and running!");
 	}
