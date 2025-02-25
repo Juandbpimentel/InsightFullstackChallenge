bd7850bf7c6b0de9e59360708bdceb03b260ab2e
Juan Pimentel <juandbpimentel@gmail.com>
2025-02-24 22:46:41 -0300

    feature: atualizando script para gerar certificados

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -4,10 +4,14 @@
 docker compose down
 
 # Gerar os certificados SSL usando o Certbot
-docker run -it --rm --name certbot \
-  -v "$(pwd)/certificates:/etc/letsencrypt" \
-  certbot/certbot certonly --standalone -d insightchallenge.juanpimentel.dev.br \
+certbot certonly --standalone -d insightchallenge.juanpimentel.dev.br \
   --staple-ocsp -m juandbpimentel@gmail.com --agree-tos
+
+# Copiar os certificados para a pasta certificates
+cp /etc/letsencrypt/live/insightchallenge.juanpimentel.dev.br/fullchain.pem ./certificates/fullchain.pem
+cp /etc/letsencrypt/live/insightchallenge.juanpimentel.dev.br/privkey.pem ./certificates/privkey.pem
+cp /etc/letsencrypt/options-ssl-nginx.conf ./certificates/options-ssl-nginx.conf
+cp /etc/letsencrypt/ssl-dhparams.pem ./certificates/ssl-dhparams.pem
 
 # Reiniciar o NGINX com os novos certificados
 docker compose up -d
