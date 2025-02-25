57104a5aab532f42c28f81d3e9c4aa9d8ddb22d3
Juan Pimentel <juandbpimentel@gmail.com>
2025-02-24 22:54:25 -0300

    feature: atualizando script para gerar certificados

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -7,6 +7,25 @@ docker compose down
 certbot certonly --standalone -d insightchallenge.juanpimentel.dev.br \
   --staple-ocsp -m juandbpimentel@gmail.com --agree-tos
 
+# Criar o arquivo options-ssl-nginx.conf se não existir
+if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
+  cat <<EOL > /etc/letsencrypt/options-ssl-nginx.conf
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_prefer_server_ciphers on;
+ssl_ciphers "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256";
+ssl_session_timeout 1d;
+ssl_session_cache shared:SSL:50m;
+ssl_session_tickets off;
+ssl_stapling on;
+ssl_stapling_verify on;
+EOL
+fi
+
+# Gerar o arquivo ssl-dhparams.pem se não existir
+if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
+  openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
+fi
+
 # Copiar os certificados para a pasta certificates
 cp /etc/letsencrypt/live/insightchallenge.juanpimentel.dev.br/fullchain.pem ./certificates/fullchain.pem
 cp /etc/letsencrypt/live/insightchallenge.juanpimentel.dev.br/privkey.pem ./certificates/privkey.pem
